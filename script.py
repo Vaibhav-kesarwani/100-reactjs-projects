ed5bdf7403c0df9f53130731477a85ddc49be1db
Vaibhav Kesarwani <116189379+Vaibhav-kesarwani@users.noreply.github.com>
2025-07-04 07:22:28 +0530

    Update script.py

--- script.py
+++ script.py
@@ -7,14 +7,13 @@ def rename_folders_with_index(path="."):
         f for f in os.listdir(path)
         if os.path.isdir(os.path.join(path, f)) and f not in excluded
     ]
-    folders.sort()  # Sort alphabetically
+    folders.sort()  
 
-    for index, folder in enumerate(folders, start=1):  # starts from 1
+    for index, folder in enumerate(folders, start=1):  
         old_path = os.path.join(path, folder)
         new_folder_name = f"{index:03d}_{folder}"
         new_path = os.path.join(path, new_folder_name)
 
-        # Skip if already renamed
         if folder.startswith(f"{index:03d}_"):
             continue
 
